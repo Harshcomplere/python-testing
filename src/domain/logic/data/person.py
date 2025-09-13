71702ddcba80d11936ccc6ac9949a920b23fc268
Harshcomplere <harsh.bhola@complereinfosystem.com>
2025-09-13 23:48:05 +0530

    change second

--- src/domain/logic/data/person.py
+++ src/domain/logic/data/person.py
@@ -1,9 +1,13 @@
 
 class Person():
     """A class representing a person with a name and age."""
-    def __init__(self, person_name: str, person_age: int):
+    def __init__(self, person_name: str, person_age: str):
         self.person_name = person_name
+        self.person_age = person_age
         
-    def get_age(self) -> int:
+    def get_name(self) -> str:
+        return f"The Person name is {self.person_name}"
+    
+    def get_age(self) -> str:
         """Get the age of the recipient"""
-        return 18
+        return f"{self.person_age}"
